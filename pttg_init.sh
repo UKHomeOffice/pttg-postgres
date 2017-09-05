b4632656d23b0a635386cd6c8fb0b2c819c9fb9a
Lucy Bennett <lucy.bennett@digital.homeoffice.gov.uk>
2017-09-05 16:04:01 +0100

    w i p

---
+++ pttg_init.sh
@@ -0,0 +1,14 @@
+
+export PGPASSWORD=${ROOT_DB_PASSWORD}
+
+
+psql -h${PTTG_DB_HOSTNAME} -Uroot -d${PTTG_DB_NAME} << EOF
+CREATE SCHEMA IF NOT EXISTS pttg_ip_dummy;
+
+#ALTER SCHEMA pttg_ip OWNER TO postgres;
+
+create user pttg with password 'pttg';
+
+grant all privileges on SCHEMA pttg_ip_dummy to pttg;
+EOF
+
