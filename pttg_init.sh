3617f3b28b47d687bea773c45265e32d6217a88b
Lucy Bennett <lucy.bennett@digital.homeoffice.gov.uk>
2017-09-06 14:26:48 +0100

    try creating new schema

--- pttg_init.sh
+++ pttg_init.sh
@@ -3,13 +3,13 @@ export PGPASSWORD=${ROOT_DB_PASSWORD}
 
 
 psql -h${PTTG_DB_HOSTNAME} -Uroot -d${PTTG_DB_NAME} << EOF
-CREATE SCHEMA IF NOT EXISTS pttg_ip_dummy;
+CREATE SCHEMA IF NOT EXISTS pttg_ip_dummy2;
 
 #ALTER SCHEMA pttg_ip OWNER TO postgres;
 
-create user pttg with password 'pttg';
+create user pttg2 with password ${IP_DB_PASSWORD};
 
-grant all privileges on SCHEMA pttg_ip_dummy to pttg;
+grant all privileges on SCHEMA pttg_ip_dummy2 to pttg2;
 EOF
 
 tail -f /dev/null
