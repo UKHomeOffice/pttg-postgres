4ac3cf5f5e90f84b4e31bd235ae7c8da245909f6
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-09-08 11:01:02 +0100

    Pttg 401 (#15)
    * Perfom DDL stuff when in bootstrap mode, then terminate. Otherwise leave the container running.
    
    * Tidy up
    
    * Remove config of the KUBE_TOKEN env variable - now done in the Kube project

--- pttg_init.sh
+++ pttg_init.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
-export PGPASSWORD=${ROOT_DB_PASSWORD}
 
+if [ "$MODE" == "bootstrap" ] ; then
 
-psql -h${PTTG_DB_HOSTNAME} -Uroot -d${PTTG_DB_NAME} << EOF
-CREATE SCHEMA IF NOT EXISTS pttg_schema;
+    export PGPASSWORD=${ROOT_DB_PASSWORD}
 
-create user pttg_user with password '${IP_DB_PASSWORD}';
+    psql -h${PTTG_DB_HOSTNAME} -Uroot -d${PTTG_DB_NAME} << EOF
 
-grant all privileges on SCHEMA pttg_schema to pttg_user;
+        CREATE SCHEMA IF NOT EXISTS pttg_schema;
+        CREATE USER pttg_user WITH PASSWORD '${IP_DB_PASSWORD}';
+        GRANT ALL PRIVILEGES ON SCHEMA pttg_schema TO pttg_user;
+
+# replace above lines with these ...
+        # CREATE SCHEMA IF NOT EXISTS ${IP_SCHEMA_NAME};
+        # CREATE USER ${IP_DB_USERNAME} WITH PASSWORD '${IP_DB_PASSWORD}';
+        # GRANT ALL PRIVILEGES ON SCHEMA ${IP_SCHEMA_NAME} TO ${IP_DB_USERNAME};
+
+        CREATE USER ${IP_DB_QUERY_USERNAME} WITH PASSWORD '${IP_DB_QUERY_PASSWORD}';
+        GRANT SELECT ON ALL TABLES IN SCHEMA ${IP_SCHEMA_NAME} TO ${IP_DB_QUERY_USERNAME};
+
 EOF
 
-tail -f /dev/null
+else
+    tail -f /dev/null
+fi
 
